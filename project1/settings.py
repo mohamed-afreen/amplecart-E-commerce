c2ccac019b8d991b094b1197a08d1ea032e08dd4
arin <afrinjasim204@gmail.com>
2023-10-11 12:59:00 +0530

    .

--- project1/settings.py
+++ project1/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'amplecart_db',
         'USER': 'postgres',
         'PASSWORD': '2468',
