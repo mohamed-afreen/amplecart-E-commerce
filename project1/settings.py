c63b9215b9f9d2cd2a5749e9f93fbc159b791d60
arin <afrinjasim204@gmail.com>
2023-10-28 23:22:48 +0530

    assets to static

--- project1/settings.py
+++ project1/settings.py
@@ -141,9 +141,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'assets/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'assets')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 
